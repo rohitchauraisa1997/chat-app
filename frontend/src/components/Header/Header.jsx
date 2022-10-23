a0552202313e78d2dc68ba50087b8a130b4faf2f
rohitchauraisa1997 <97086931+RohitChaurasia97@users.noreply.github.com>
2022-10-23 23:27:25 +0900

    Header Component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "./Header.scss"
+
+const Header = () => (
+    <div classNmae="header">
+        <h2> Realtime Chat Application </h2>
+    </div>
+);
+
+export default Header;
